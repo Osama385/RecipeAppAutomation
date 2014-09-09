402b0df0f9751ab826966e3d99a45da79a7eac7e
Osama385 <osama.altaf@ebricks-inc.com>
2014-09-09 19:17:06 +0500

    Commit - tuesday 9 September

--- src/testcases/EditRecipe.java
+++ src/testcases/EditRecipe.java
@@ -100,7 +100,7 @@ public class EditRecipe {
 									webs.findElement(By.xpath(xp.Ingredient_Name_Field)).sendKeys(R_IngName);
 									System.out.println("Ingredient Name is added successfully");
 									
-									try {
+									/*try {
 										Thread.sleep(5000);
 									} catch (InterruptedException e) {
 										// TODO Auto-generated catch block
@@ -108,11 +108,12 @@ public class EditRecipe {
 										webs.quit();
 									}
 									System.out.println(R_IngAmnt);
+									
 									webs.findElement(By.xpath(xp.Ingredient_Amount_Field)).sendKeys(R_IngAmnt);
 									
 									
 									System.out.println("Ingredient name and amount is entered successfully");
-							
+							*/
 									if(ut.IsElementPresent(xp.Ingredient_Save_Button, webs))
 									{
 										webs.findElement(By.xpath(xp.Ingredient_Save_Button)).click();
