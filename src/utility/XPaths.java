3df3c0914fb019ef414988f9f2759d4fa4843b3b
Osama385 <osama.altaf@ebricks-inc.com>
2014-09-15 21:34:21 +0500

    Commit#4 - 15th September 2014

--- src/utility/XPaths.java
+++ src/utility/XPaths.java
@@ -5,7 +5,7 @@ public class XPaths {
 	
 	//XPATHs for Add Recipe
 	
-	//public String Add_button = "//UIANavigationBar/descendant::UIAButton[@name='Add']"; // Add button Xpath
+	public String Add_button = "//UIANavigationBar/descendant::UIAButton[@name='Add']"; // Add button Xpath
 	
 	public String Recipe_Name_Field = "//UIATextField[@value='Recipe Name']";
 	
