4f85a9fff71dc9f0243dc15f79660dee947e494d
Osama385 <osama.altaf@ebricks-inc.com>
2014-09-23 15:49:32 +0500

    23rd September commit -- Ant file commit

--- src/testcases/AddCategory.java
+++ src/testcases/AddCategory.java
@@ -2,7 +2,7 @@ package testcases;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileDriver;
-
+import io.appium.java_client.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
@@ -36,54 +36,56 @@ public class AddCategory {
 		Utility ut = new Utility();
 		String v1 = xp.Check_Recipe_In_List1.replace("##MARKER12", Recipe_name);
 		
-		if(ut.IsElementPresent(v1, webs))
+		if(ut.IsElementPresent(v1, md))
 		{
 			md.findElement(By.xpath(v1)).click();
 			System.out.println("Recipe" + Recipe_name + " is found in the List");
 			
 			//String v2 = xp.Check_Recipe_Field_Value.replace("##MARKER11", Recipe_name);
 			
-			if(ut.IsElementPresent(xp.Edit_Recipe_Button, webs))
+			if(ut.IsElementPresent(xp.Edit_Recipe_Button, md))
 			{
 				System.out.println("Edit Recipe Screen is appeared successfully");
 				md.findElement(By.xpath(xp.Edit_Recipe_Button)).click();
 				System.out.println("Edit Recipe Button is clicked successfully");
 				
-				if(ut.IsElementPresent(xp.Recipe_Cat_MoreInfo_button, webs))
+				if(ut.IsElementPresent(xp.Recipe_Cat_MoreInfo_button, md))
 				{
 					md.findElement(By.xpath(xp.Recipe_Cat_MoreInfo_button)).click();
 					System.out.println("More Info Category button is clicked successfully");
 					
-					if(ut.IsElementPresent(xp.Verify_Category_Screen, webs))
+					if(ut.IsElementPresent(xp.Verify_Category_Screen, md))
 					{
 						System.out.println("Category Selection screen is appeared successfully");
 						
 						String v3 = xp.Category_of_Recipe.replace("##MARKER13", Recipe_cat);
 						
-						if(ut.IsElementPresent(v3, webs))
+						if(ut.IsElementPresent(v3, md))
 						{
 							md.findElement(By.xpath(v3)).click();
 							System.out.println(Recipe_cat + "category is selected successfully");
 							
-							if(ut.IsElementPresent(xp.Done_Button_onCategroy, webs))
+							if(ut.IsElementPresent(xp.Done_Button_onCategroy, md))
 							{
 								md.findElement(By.xpath(xp.Done_Button_onCategroy)).click();
 								System.out.println("Done button is found on Category screen and clicked successfully");
 								
 								String v4 = xp.Chk_Cat_Selected.replace("##MARKER14", Recipe_cat);
 								
-								if(ut.IsElementPresent(v4, webs))
+								if(ut.IsElementPresent(v4, md))
 								{
 									System.out.println("Category is selected and added successfully");
 									
-									if(ut.IsElementPresent(xp.Done_Button_onEditScreen, webs))
+									if(ut.IsElementPresent(xp.Done_Button_onEditScreen, md))
 									{
 										md.findElement(By.xpath(xp.Done_Button_onEditScreen)).click();
 										System.out.println("Done button is found and clicked from Edit Recipe Screen");
 										
-										if(ut.IsElementPresent(xp.Return2Rec_Button, webs))
+										if(ut.IsElementPresent(xp.Return2Rec_Button, md))
 										{
 											md.findElement(By.xpath(xp.Return2Rec_Button)).click();
+											
+											//((MobileElement) md.findElement(By.xpath(xp.Return2Rec_Button))).
 											
 											System.out.println("****Edit functionality for adding Category is working successfully ****");
 											
@@ -98,6 +100,7 @@ public class AddCategory {
 									else
 									{
 										System.err.println("Done button on Edit Recipe screen is not found");
+										md.quit();
 									}
 								}
 								else
