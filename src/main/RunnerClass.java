32123a26ccb7305ad3901bb896a78907d8cd2ca4
Osama385 <osama.altaf@ebricks-inc.com>
2015-05-21 10:00:46 +0500

    test commit

--- src/main/RunnerClass.java
+++ src/main/RunnerClass.java
@@ -41,7 +41,7 @@ public class RunnerClass {
 			e.printStackTrace();
 		}
 		
-		//String lWorkDir = new String(System.getProperty("user.dir"));
+		//String lWorkDir = new String(System.getProperty("user.dir")); //test code
 		ExcelReader excelFile = new ExcelReader("AddRecipe.xlsx");
 		
 		XPaths xp = new XPaths();
